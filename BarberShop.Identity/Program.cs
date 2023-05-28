4a3695c898c8fe1410c6e17010f9f09d55c1e42f
AydoanB <balakchiev02@gmail.com>
2023-05-28 18:40:11 +0300

    Refactor Program file

--- BarberShop.Identity/Program.cs
+++ BarberShop.Identity/Program.cs
@@ -10,20 +10,39 @@ namespace BarberShop.Identity
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-        }
 
-        public IConfiguration Configuration { get; }
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
-        public void ConfigureServices(IServiceCollection services)
-            => services
-                .AddWebService<IdentityDbContext>(this.Configuration)
+            builder.Services
+                .AddWebService<IdentityDbContext>(builder.Configuration)
                 .AddUserStorage()
+                .AddRouting(opt => opt.LowercaseUrls = true)
                 .AddTransient<IIdentityService, IdentityService>()
                 .AddTransient<ITokenGeneratorService, TokenGeneratorService>();
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-            => app
-                .UseWebService(env)
-                .Initialize();
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app
+                .UseHttpsRedirection()
+                .UseRouting()
+                .UseCors(options => options
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod())
+                .UseAuthentication()
+                .UseAuthorization()
+                .UseEndpoints(endpoints => endpoints
+                    .MapControllers());
+
+            app.Run();
+        }
     }
 }
