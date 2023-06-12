8aac8e69c69232b4b36a59938f3c6eb7761d6f3f
Aydoan Balakchiev <balakchiev02@gmail.com>
2023-06-12 09:27:17 +0300

    Modify consumer object to retrieve userid

--- BarberShop.Appointments/Messages/UserBarberCreatedConsumer.cs
+++ BarberShop.Appointments/Messages/UserBarberCreatedConsumer.cs
@@ -1,7 +1,7 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-using BarberShop.Appointments.Models.Users;
+﻿using BarberShop.Appointments.Models.Users;
 using BarberShop.Appointments.Services;
 using BarberShop.Messages;
+using BarberShop.Services;
 using MassTransit;
 
 namespace BarberShop.Appointments.Messages;
@@ -9,24 +9,24 @@ namespace BarberShop.Appointments.Messages;
 public class UserBarberCreatedConsumer : IConsumer<BarberCreatedMessage>
 {
     private readonly IBarberService _barberService;
+    private readonly ICurrentUserService _currentUserService;
 
-    public UserBarberCreatedConsumer(IBarberService barberService)
+    public UserBarberCreatedConsumer(
+        IBarberService barberService, 
+        ICurrentUserService currentUserService)
     {
         _barberService = barberService;
+        _currentUserService = currentUserService;
     }
 
     public async Task Consume(ConsumeContext<BarberCreatedMessage> context)
     {
-        var jwt = context.Message.Token;
-        var handler = new JwtSecurityTokenHandler();
-        var token = handler.ReadJwtToken(jwt);
-
-        var userId = token.Claims.FirstOrDefault(claim => claim.Type == "nameid")?.Value;
+        var currentUserId = _currentUserService.UserId;
 
         var barber = new NewBarberDto
         {
             Name = context.Message.Name,
-            //UserId = userId
+            UserId = currentUserId
         };
 
         await _barberService.CreateUser(barber);
